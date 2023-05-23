9e9be466927e0e54bcc0864800d4e22f53b01050
anzeera <anzeerapp@gmail.com>
2023-05-23 15:59:57 +0530

    commit

---
+++ bin/fina_const.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name ="manu";
+  int age = 20;
+  final String course;
+  course = "flutter";
+  const String batch = "python";
+}
---
+++ bin/var,dynamicdart.dart
@@ -0,0 +1,7 @@
+void main() {
+  var name = "abhay";
+  dynamic age = 20;
+  age = "abhay";
+  print(name);
+  print(age);
+}
