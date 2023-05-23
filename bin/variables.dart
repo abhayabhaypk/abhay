882bdcd385ac833cd92fd410540e0209f9542ca5
anzeera <anzeerapp@gmail.com>
2023-05-23 15:17:36 +0530

    commit

---
+++ bin/variables.dart
@@ -0,0 +1,14 @@
+void main() {
+  String name = "abhay";
+  int age = 20;
+  double mark=8.5;
+  String course = "flutter";
+
+
+  print("my name ${name}");
+  print("my age is $age");
+  print("my mark is $mark");
+  print("my course $course");
+
+  //${}
+}
