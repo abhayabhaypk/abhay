733a1e279977c698c15d413f897455a04dce4c4e
anzeera <anzeerapp@gmail.com>
2023-05-24 14:52:08 +0530

    commit

---
+++ bin/arithametidart.dart
@@ -0,0 +1,12 @@
+void main(){
+  var a=20;
+  var b=10;
+
+  print("a+b =${a+b}");
+  print('a-b =${a-b}');
+  print('a*b =${a*b}');
+  print('a/b =${a/b}');
+  print('a~/b =${a~/b}');
+  print('a%b =${a%b}');
+  print('-(a+b)=${-(a+b)}');
+}
