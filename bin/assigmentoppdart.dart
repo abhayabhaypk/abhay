02a0056b0c1248cbcba4589f64adee458b471b3c
anzeera <anzeerapp@gmail.com>
2023-05-24 15:08:23 +0530

    commit

---
+++ bin/assigmentoppdart.dart
@@ -0,0 +1,10 @@
+void main(){
+  dynamic x=100,y=10;
+  print('x=y ${x = y}'); // x=10
+  print('x+=y ${x+=y}'); //x=20
+  print('x-=y ${x-=y}'); //x=10
+  print('x*=y ${x*=y}'); //x=100
+  print('x/=y ${x/=y}');
+  print('x~/=y ${x~/y}');
+  print('x%=y ${x%=y}');
+}
