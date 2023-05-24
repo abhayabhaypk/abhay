c11b397cd0dc5ebb4088a2c76b35a95d944b41f9
anzeera <anzeerapp@gmail.com>
2023-05-24 15:55:58 +0530

    commit

---
+++ bin/logicalopp.dart
@@ -0,0 +1,8 @@
+void main(){
+  String uername="admin";
+  int password= 123456;
+  print(uername=="admin" && password==123456);
+  print(uername=="admin" || password==12345);
+  print(!( uername=="admin" ||password==123456));
+
+}
---
+++ bin/typrtestopp.dart
@@ -0,0 +1,7 @@
+void main(){
+  var name="abhay";
+  int age=10;
+  print(name is int);
+  print(age is! int);
+  
+}
