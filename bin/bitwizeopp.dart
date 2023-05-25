d2f061b1e35ed20cc0af06fbacd148462ee5e2da
anzeera <anzeerapp@gmail.com>
2023-05-25 15:55:12 +0530

    commit

---
+++ bin/bitwizeopp.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  //logical
+  String name ="admin";
+  print(uname is! int);
+
+  int a= 7; //0111
+  int  b=5; //0101
+
+  //| 0101
+
+  //^0111
+
+  //^  0010
+  print(a & b);
+  print(a | b);
+  print(a ^ b);
+
+}
---
+++ bin/conditionalopp.dart
@@ -0,0 +1,14 @@
+void main(){
+  String uername="admin";
+  int password=123456;
+
+  var result=uername=="admin" && password==123456 ? "Login suceessful" : "Login failuer";
+  print(result);
+
+}
+
+
+
+
+
+
